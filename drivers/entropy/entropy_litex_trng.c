fe7ae9b1ec40c943fd82f58da88771ddec3c4a60
Kaz Kojima <kkojima@rr.iij4u.or.jp>
2021-02-18 16:05:41 +0900

    drivers: entropy: entropy_litex_trng.c: Update for crypto libs.

--- drivers/entropy/entropy_litex_trng.c
+++ drivers/entropy/entropy_litex_trng.c
@@ -8,11 +8,65 @@
 #include <string.h>
 #include <zephyr.h>
 
+#if defined(CONFIG_WOLFSSL)
+typedef struct wc_Sha256 {
+        uint8_t digest[32];
+        uint8_t buffer[64];
+        uint32_t buffLen;   /* in bytes          */
+        uint32_t loLen;     /* length in bytes   */
+        uint32_t hiLen;     /* length in bytes   */
+        void* heap;
+} wc_Sha256_t;
+#define SHA256_BLOCK_SIZE 64
+#define SHA256_DIGEST_SIZE 32
+#define sha256_init wc_InitSha256
+#define sha256_update wc_Sha256Update
+static inline void sha256_final(uint8_t *digest, wc_Sha256_t *str)
+{
+	wc_Sha256Final(str, digest);
+}
+static wc_Sha256_t sha256_struct;
+#elif defined(CONFIG_MBEDTLS)
+#include <mbedtls/sha256.h>
+#define SHA256_BLOCK_SIZE 64
+#define SHA256_DIGEST_SIZE 32
+static inline void sha256_init(mbedtls_sha256_context *str)
+{
+	mbedtls_sha256_init(str);
+	mbedtls_sha256_starts_ret(str, 0);
+}
+#define sha256_update mbedtls_sha256_update_ret
+#define sha256_final mbedtls_sha256_finish_ret
+static mbedtls_sha256_context sha256_struct;
+#elif defined(CONFIG_TINYCRYPT)
 #include <tinycrypt/sha256.h>
-
+#define SHA256_BLOCK_SIZE TC_SHA256_BLOCK_SIZE
+#define SHA256_DIGEST_SIZE TC_SHA256_DIGEST_SIZE
+#define sha256_init tc_sha256_init
+#define sha256_update tc_sha256_update
+#define sha256_final tc_sha256_final
 static struct tc_sha256_state_struct sha256_struct;
-static uint8_t sha256_block[TC_SHA256_BLOCK_SIZE];
-static uint8_t sha256_digest[TC_SHA256_DIGEST_SIZE];
+#else
+// No software conditioning
+typedef struct sha256_context {
+	uint8_t buffer[32];
+} sha256_context_t;
+#define SHA256_BLOCK_SIZE 32
+#define SHA256_DIGEST_SIZE 32
+static inline void sha256_init(sha256_context *str) {}
+static inline void sha256_update(sha256_context_t *str, uint8_t *s, size_t len)
+{
+	memcpy(srt->buffer, s, len);
+}
+static inline void sha256_final(uint8_t *digest, sha256_context_t *str)
+{
+	memcpy(digest, srt->buffer, SHA256_DIGEST_SIZE);
+}
+static sha256_context_t sha256_struct;
+#endif
+
+static uint8_t sha256_block[SHA256_BLOCK_SIZE];
+static uint8_t sha256_digest[SHA256_DIGEST_SIZE];
 static size_t sha256_offset;
 
 #define TRNG_STATUS     ((volatile uint32_t *)DT_INST_REG_ADDR(0) + 1)
@@ -23,13 +77,13 @@ static void fill_digest(void)
 {
 	size_t i, j;
 
-	for (i = 0; i < TC_SHA256_BLOCK_SIZE/4; i++) {
+	for (i = 0; i < SHA256_BLOCK_SIZE/4; i++) {
 		for (j = 0; j < TRNG_WIDTH; ++j) {
 			sha256_block[4*i+j] = (uint8_t)*(TRNG_STATUS + j);
 		}
 	}
-	tc_sha256_update(&sha256_struct, sha256_block, TC_SHA256_BLOCK_SIZE);
-	tc_sha256_final(sha256_digest, &sha256_struct);
+	sha256_update(&sha256_struct, sha256_block, SHA256_BLOCK_SIZE);
+	sha256_final(sha256_digest, &sha256_struct);
 	sha256_offset = 0;
 }
 
@@ -39,11 +93,11 @@ static int entropy_trng_get_entropy(const struct device *dev, uint8_t *buffer,
 	while (length > 0) {
 		size_t to_copy;
 
-		to_copy = MIN(length, (TC_SHA256_DIGEST_SIZE - sha256_offset));
+		to_copy = MIN(length, (SHA256_DIGEST_SIZE - sha256_offset));
 		memcpy(buffer, &sha256_digest[sha256_offset], to_copy);
 		buffer += to_copy;
 		length -= to_copy;
-		if (to_copy + sha256_offset == TC_SHA256_DIGEST_SIZE) {
+		if (to_copy + sha256_offset == SHA256_DIGEST_SIZE) {
 			fill_digest();
 		} else
 			sha256_offset += to_copy;
@@ -53,7 +107,7 @@ static int entropy_trng_get_entropy(const struct device *dev, uint8_t *buffer,
 
 static int entropy_trng_init(const struct device *dev)
 {
-	tc_sha256_init(&sha256_struct);
+	sha256_init(&sha256_struct);
 	fill_digest();
 	return 0;
 }
